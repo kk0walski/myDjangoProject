ac7f195d6bd8e26390cf9502596161f6557a8335
Karol <karol.kowalski95@gmail.com>
2017-09-26 19:16:31 +0200

    Utworzenie menagera modelu

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
 
 # Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Roboczy'),
@@ -17,6 +21,8 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ('-publish',)

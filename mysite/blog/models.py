f7121d49a18c0eb739ddb2259b28e95770a62fa0
Karol <karol.kowalski95@gmail.com>
2017-09-26 19:31:30 +0200

    Dodanie kanonicznego adresu

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 
@@ -23,6 +24,13 @@ class Post(models.Model):
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
     objects = models.Manager()
     published = PublishedManager()
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                        args=[self.publish.year,
+                              self.publish.strftime('%m'),
+                              self.publish.strftime('%d'),
+                              self.slug])
 
     class Meta:
         ordering = ('-publish',)

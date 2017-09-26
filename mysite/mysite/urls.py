67a38f8c200ae14725de83a31799dfb1f5a6b0a8
Karol <karol.kowalski95@gmail.com>
2017-09-26 19:28:27 +0200

    Adding blog url

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
+    url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog'))
 ]

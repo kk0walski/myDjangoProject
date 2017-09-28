52b21415345a8a92a0c219be8d83f7bfe93ba391
Karol <karol.kowalski95@gmail.com>
2017-09-28 14:27:30 +0200

    Refaktoryzacja widoków

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import url
 from . import views
+from .views import TagIndexView, PostDetail
 
 urlpatterns = [
     # Widoki posta
     #url(r'^$', views.post_list, name='post_list'),
     url(r'^$', views.PostListView.as_view(), name='post_list'),
+    url(r'^tag/(?P<slug>[-\w]+)/$', TagIndexView.as_view(), name='tagged'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'\
-        r'(?P<post>[-\w]+)/$',
-        views.post_detail,
+        r'(?P<slug>[-\w]+)/$',
+        PostDetail.as_view(),
         name='post_detail'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,21 +1,33 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.views.generic import ListView
+from taggit.models import Tag
+from django.views.generic import ListView, DetailView
+
+class TagMixin(object):
+    def get_context_data(self, **kwargs):
+        context = super(TagMixin, self).get_context_data(**kwargs)
+        context['tags'] = Tag.objects.all()
+        return context
 
 # Create your views here.
-class PostListView(ListView):
+class PostListView(TagMixin, ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
     paginate_by = 3
     temlate_name = 'blog/post_list.html'
+    model = Post
 
-def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post,
-                             slug=post,
-                             status='published',
-                             publish__year=year,
-                             publish__month=month,
-                             publish__day=day)
-    return render(request,
-                  'blog/post/detail.html',
-                  {'post':post})
+class PostDetail(DetailView):
+    template_name = 'blog/post/detail.html'
+    context_object_name = 'post'
+    model = Post
+    slug_field = 'slug'
+
+class TagIndexView(TagMixin, ListView):
+    template_name = 'blog/post_list.html'
+    model = Post
+    paginate_by = 3
+    context_object_name = 'posts'
+
+    def get_querystet(self):
+        return Post.objects.filter(tags__slug=self.kwargs.get('slug'))
